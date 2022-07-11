6806e402e2d8128b0666651e6eaca0febe6606bc
Benjamin Leguizamon <leguizamonbenjamin0@gmail.com>
2022-07-11 10:51:36 -0300

    Web de la inmobiliaria sin la validación del formulario

---
+++ javascript/arrayCasas.js
@@ -0,0 +1,202 @@
+let casas = [
+    {
+        barrio: "Mendiolaza",
+        habitaciones: 3,
+        banios: 2,
+        precio: 270000,
+        mts2: 1675,
+        image: "../imagenes/mendiolaza-1.jpg",
+        id: 1,
+    },
+    {
+        barrio: "Zona sur",
+        habitaciones: 4,
+        banios: 4,
+        precio: 750000,
+        mts2: 1028,
+        image: "../imagenes/zona-sur-1.jpg",
+        id: 2,
+    },
+    
+    {
+        barrio: "Mendiolaza",
+        habitaciones: 4,
+        banios: 3,
+        precio: 550000,
+        mts2: 1000,
+        image: "../imagenes/mendiolaza-2.jpg",
+        id: 3,
+    },
+    
+    {
+        barrio: "Zona norte",
+        habitaciones: 3,
+        banios: 3,
+        precio: 580000,
+        mts2: 800,
+        image: "../imagenes/zona-norte-1.jpg",
+        id: 4,
+    },
+    
+    {
+        barrio: "Zona sur",
+        habitaciones: 3,
+        banios: 3,
+        precio: 510000,
+        mts2: 1100,
+        image: "../imagenes/zona-sur-2.jpg",
+        id: 5,
+    },
+    
+    {
+        barrio: "Zona norte",
+        habitaciones: 2,
+        banios: 2,
+        precio: 245000,
+        mts2: 510,
+        image: "../imagenes/zona-norte-2.jpg",
+        id: 6,
+    },
+    
+    {
+        barrio: "Zona chateau",
+        habitaciones: 3,
+        banios: 3,
+        precio: 325000,
+        mts2: 740,
+        image: "../imagenes/zona-chateau-1.jpg",
+        id: 7,
+    },
+    
+    {
+        barrio: "Zona sur",
+        habitaciones: 5,
+        banios: 4,
+        precio: 400000,
+        mts2: 1192,
+        image: "../imagenes/zona-sur-3.jpg",
+        id: 8,
+    },
+    
+    {
+        barrio: "Zona norte",
+        habitaciones: 3,
+        banios: 2,
+        precio: 149000,
+        mts2: 280,
+        image: "../imagenes/zona-norte-3.jpg",
+        id: 9,
+    },
+    
+    {
+        barrio: "Zona chateau",
+        habitaciones: 5,
+        banios: 5,
+        precio: 700000,
+        mts2: 1500,
+        image: "../imagenes/zona-chateau-2.jpg",
+        id: 10,
+    },
+    
+    {
+        barrio: "Zona chateau",
+        habitaciones: 5,
+        banios: 3,
+        precio: 650000,
+        mts2: 1500,
+        image: "../imagenes/zona-chateau-3.jpg",
+        id: 11,
+    },
+    
+    {
+        barrio: "Zona sur",
+        habitaciones: 4,
+        banios: 4,
+        precio: 450000,
+        mts2: 403,
+        image: "../imagenes/zona-sur-4.jpg",
+        id: 12,
+    },
+
+    {
+        barrio: "Zona chateau",
+        habitaciones: 2,
+        banios: 2,
+        precio: 195000,
+        mts2: 211,
+        image: "../imagenes/zona-chateau-4.jpg",
+        id: 13,
+    },
+    
+    {
+        barrio: "Zona chateau",
+        habitaciones: 3,
+        banios: 3,
+        precio: 350000,
+        mts2: 620,
+        image: "../imagenes/zona-chateau-5.jpg",
+        id: 14,
+    },
+    
+    {
+        barrio: "Zona norte",
+        habitaciones: 4,
+        banios: 3,
+        precio: 260000,
+        mts2: 405,
+        image: "../imagenes/zona-norte-4.jpg",
+        id: 15,
+    },
+    
+    {
+        barrio: "Nueva cordoba",
+        habitaciones: 1,
+        banios: 1,
+        precio: 68000,
+        mts2: 61,
+        image: "",
+        id: 16,
+    },
+    
+    {
+        barrio: "Nueva cordoba",
+        habitaciones: 1,
+        banios: 1,
+        precio: 47800,
+        mts2: 58,
+        image: "",
+        id: 17,
+    },
+    
+    {
+        barrio: "Nueva cordoba",
+        habitaciones: 2,
+        banios: 1,
+        precio: 115000,
+        mts2: 75,
+        image: "",
+        id: 18,
+    },
+    
+    {
+        barrio: "Nueva cordoba",
+        habitaciones: 3,
+        banios: 3,
+        precio: 325000,
+        mts2: 350,
+        image: "",
+        id: 19,
+    },
+    
+    {
+        barrio: "Villa allende",
+        habitaciones: 5,
+        banios: 4,
+        precio: 450000,
+        mts2: 1600,
+        image: "../imagenes/villa-allende.jpg",
+        id: 20,
+    },
+] 
+
+export { casas };
---
+++ javascript/main.js
@@ -0,0 +1,171 @@
+import {casas} from "./arrayCasas.js"
+
+/*--------Filtro de casas--------*/ 
+const selectorBarrio = document.getElementById("selecBarrio");
+
+selectorBarrio.addEventListener("change", () => {
+    console.log(selectorBarrio.value);
+    if(selectorBarrio.value=="all"){
+        mostrarCasas (casas);
+    } else {
+        mostrarCasas(casas.filter(elemento => elemento.barrio == selectorBarrio.value));
+    }
+})
+
+/*--------Renderizado de casas--------*/ 
+const contenedorCasas = document.getElementById("contenedor-cards");
+
+function mostrarCasas (array) {
+    contenedorCasas.innerHTML = "";
+    array.forEach(el => {
+        let div = document.createElement("div");
+        div.className = `container`;
+        div.className = `col-md-3`;
+        div.innerHTML = `<div class="card">
+                            <img src="${el.image}" alt="Casa" class="img-fluid">
+                            <h4 class="text-center my-3 text-uppercase">${el.barrio}</h4>
+                            <p class="text-center">Habitaciones: ${el.habitaciones} <br>
+                                                    Baños: ${el.banios} <br>
+                                                    Mts2: ${el.mts2} <br>
+                                                    Precio: $${el.precio}</p>
+                            <div class="centrar-boton">
+                                <a id="boton${el.id}" class="boton-agregar">Quiero visitarla!</a>
+                            </div>
+                        </div>`
+        
+        contenedorCasas.appendChild(div);
+
+        // Le agrego el evento a cada boton para agregar a la agenda
+        let btnAgregar = document.getElementById(`boton${el.id}`);
+        btnAgregar.addEventListener("click", () => {
+            agregarAgenda(el.id);
+            Toastify ({
+                text: "Casa añadida con éxito!",
+                duration: 2500,
+                gravity: "bottom",
+                position: "right",
+                //Modificar el background color para que quede bien con la página!
+            }).showToast();
+        })
+
+    });
+}
+mostrarCasas(casas);
+
+
+//Creo un array vacio de la agenda
+let agendaVisitas = [];
+
+
+//Funcion agregar a la agenda
+function agregarAgenda(id) {
+    let casaAgregar = casas.find(item => item.id == id);
+    agendaVisitas.push(casaAgregar);
+    mostrarCasasEnModal(casaAgregar); // A medida que se va agregando la casa al array, se la muestra en la agenda.
+    actualizarAgenda();
+    localStorage.setItem("agenda", JSON.stringify(agendaVisitas));
+}
+
+
+//Renderizar las casas que se agregan a la agenda
+
+let contenedorAgenda = document.getElementById("contenedor-agenda")
+
+function mostrarCasasEnModal (casaAgregar) {
+    let div = document.createElement(`div`);
+    div.innerHTML = `<div class="row container agenda-flex">
+                        <div class="col-md-3">
+                            <img src="${casaAgregar.image}" alt="Casa" class="img-fluid img-agenda">
+                        </div>
+                        <div class="col-md-3">
+                            <h3 class="titulo-agenda">${casaAgregar.barrio}</h3>
+                            <p>Habitaciones: ${casaAgregar.habitaciones} <br>
+                                Baños: ${casaAgregar.banios} <br>
+                                Mts2: ${casaAgregar.mts2} <br>
+                                precio:$${casaAgregar.precio}</p>
+                        </div>
+                        <div class="col-md-3">
+                            <h5 class="fecha-titulo">Seleccione una fecha para la visita</h5>
+                            <input type="date" name="Calendario" min="2022/07/06" max="2023/01/01" id="nada" class="calendario">
+                            <div class="row botones-agenda">
+                                <div><button class="btn-confirmar elemento-a-reemplazar" id="btn-confirmar-${casaAgregar.id}">Confirmar</button></div>
+                                <div><button class="btn-eliminar" id="btn-eliminar-${casaAgregar.id}" dataset-item="item">Eliminar cita</button></div>
+                            </div>
+                        </div>
+                    </div>`
+
+    contenedorAgenda.appendChild(div);
+    
+    
+    //Boton para eliminar una casa del modal
+    let btnEliminar = document.getElementById(`btn-eliminar-${casaAgregar.id}`);
+    btnEliminar.addEventListener("click", () => {
+        btnEliminar.parentElement.parentElement.parentElement.parentElement.remove(); // Tiene que haber una mejor forma de hacer esto en vez de escribir parentElement 4 veces.
+        agendaVisitas = agendaVisitas.filter(el => el.id !== casaAgregar.id)
+        actualizarAgenda();
+        localStorage.setItem(`agenda`, JSON.stringify(agendaVisitas));
+    })
+
+    //Boton para confirmar una fecha de visita!
+    // const btnConfirmarFecha = document.getElementById(`btn-confirmar-${casaAgregar.id}`);
+    // const dateControl = document.querySelector(`input[type="date"]`);
+    // const elementoAReemplazar = document.querySelectorAll(".elemento-a-reemplazar");
+    // btnConfirmarFecha.addEventListener("click", () => {
+    //     console.log(dateControl.value);
+    //     elementoAReemplazar.innerText = "Cambiar fecha";
+    // })  
+    const btnConfirmarFecha = document.getElementById(`btn-confirmar-${casaAgregar.id}`);
+    btnConfirmarFecha.addEventListener("click", ()=>{
+        swal({
+            title: "Listo!",
+            text: "Se agendó la visita para la fecha seleccionada",
+            icon: "success",
+            showConfirmButton: false,
+        }
+        )
+    })
+}
+
+
+//Funcion de actualizar la agenda (el numerito que se ve en la página principal)
+let contadorAgenda = document.getElementById("contador-agenda");
+function actualizarAgenda() {
+    contadorAgenda.innerText= "("+agendaVisitas.length+")";
+}
+
+
+//Función de recuperar todo lo guardado cuando refresco la página
+function recuperar () {
+    let recuperarLS = JSON.parse(localStorage.getItem("agenda"))||[];
+    for (const elemento of recuperarLS) {
+        mostrarCasasEnModal(elemento);
+        agendaVisitas.push(elemento);
+        actualizarAgenda();
+    }
+}
+recuperar();
+
+// Modal
+
+const modal = document.querySelector(".ventana-emergente");
+const openModal = document.getElementById("abrir-modal");
+const closeModal = document.querySelector(".modal-close");
+
+openModal.addEventListener("click", () => {
+    modal.classList.toggle("modal-show");
+});
+
+closeModal.addEventListener("click", () => {
+    modal.classList.toggle("modal-show");
+})
+
+//Sweet alert al boton de enviar formulario
+const botonFormulario = document.querySelector(".boton-contacto");
+botonFormulario.addEventListener("click", ()=>{
+    swal({
+        title: "Genial",
+        text: "Tu consulta ha sido enviada con éxito!",
+        icon: "success",
+        showConfirmButton: false,
+    })
+})
